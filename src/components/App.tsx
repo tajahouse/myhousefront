981ffeab6201e9760b10678d13adc4df9a31209b
tajahouse <ahlliahtaja@yahoo,com>
2023-05-20 17:35:41 +0200

    Setting up landing page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,42 @@ import React from 'react';
 import Header from './Header.tsx';
 import LandingPage from './LandingPage.tsx';
 import '../styles/App.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendBaseTheme, Box, Center, extendTheme } from '@chakra-ui/react';
+
+const theme = extendBaseTheme({
+  components: {
+    Box
+  }  
+})
+
+
+const themeStyle = extendTheme({
+  colors: {
+    blue: "soft blue",
+    lightblue: "light cornflower blue",
+    crystalblue: "cystal blue",
+    periwinkle: "lightSteelBlue",
+    purple: "purple sage bush"
+  }
+})
 
 const App = () =>{
   return(
-    <ChakraProvider>
+    <ChakraProvider 
+      theme={theme}
+      >
         <Header/>
-        <LandingPage/>
+        <Box 
+          h={592}
+          w="70%" 
+          alignItems = "center" 
+          bg = {themeStyle.colors.periwinkle}
+          display = "flex"
+          justifySelf= "center"
+          borderRadius = "35px"
+          >
+          <LandingPage themeStyle = {themeStyle.colors}/>
+        </Box>
     </ChakraProvider>
   );
 }
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Typewriter from 'typewriter-effect';
 
-const LandingPage = () =>{
+
+  type ThemeStyle = {
+    themeStyle: object;
+  }
+
+const LandingPage: React.FunctionComponent<ThemeStyle> = (props) =>{
+    const colors = props.themeStyle;
     return(
         <div>
             <span className='typewriter_intro'>
-            <p>Welcome to My House app where you can</p>
+            <p
+            >Welcome to My House app where you can</p>
             <Typewriter
              options={{
               strings: ['organize family events', 'keep track of schedules', 'assign chores', 'set up allowances', 'communicate safely'],
@@ -13,7 +20,7 @@ const LandingPage = () =>{
               loop: true,
               cursorClassName: 'typewriter',
               skipAddStyles: true,
-              delay: 30
+              delay: 30,
                       }}
             />
       </span>
